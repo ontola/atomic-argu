8e1e1011f51236ec12315a3f76f885138458d916
Joep Meindertsma <joep@ontola.io>
2023-01-10 15:59:33 +0100

    Updates

---
+++ _internal/immutable/chunks/2-3c48e39f.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-9ac8eaef.js";import{default as t}from"../components/pages/_page.svelte-afb5bc32.js";export{t as component,r as universal};
--- internal/immutable/chunks/1-2872a442.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-e43a0b24.js";export{t as component};
