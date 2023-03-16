3da6aacfade7cc2a7c8761d7206fbfc5bb7ffa83
Joep Meindertsma <joep@ontola.io>
2023-03-16 16:10:43 +0100

    Prettier cards

--- src/lib/components/ArticleCard.svelte
+++ src/lib/components/ArticleCard.svelte
@@ -42,10 +42,12 @@
 		border-radius: var(--t-radius);
 		height: 12rem;
 		transition: transform 300ms var(--ease-squish-1), box-shadow 100ms ease-in-out;
+		/* Prevent border radius on images */
+		overflow: hidden;
 	}
 	.card:hover,
 	.card:focus {
-		box-shadow: inset 0 0 0 2px var(--t-color-main);
+		box-shadow: 0 0 0 2px var(--t-color-main);
 		outline: none;
 		transform: scale(1.05);
 	}
